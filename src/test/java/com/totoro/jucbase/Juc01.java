c119631b9f94dcded2020df383ec7b4bedf82d79
totoro <1026215903@qq.com>
2023-01-29 16:28:33 +0800

    juc基础

--- src/test/java/com/totoro/jucbase/Juc01.java
+++ src/test/java/com/totoro/jucbase/Juc01.java
@@ -12,9 +12,15 @@ public class Juc01 {
     public static void main(String[] args) {
         int[] arr = new int[]{3,1,5,2,4};
 
-        Arrays.sort(args);
         for (int i : arr) {
-            System.out.println(i);
+            new Thread(() -> {
+                try {
+                    Thread.sleep(i * 1000);
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
     }
 }
---
+++ src/test/java/com/totoro/jucbase/Juc02.java
@@ -0,0 +1,15 @@
+package com.totoro.jucbase;
+
+/**
+ * @author:totoro
+ * @createDate:2023/1/29
+ * @description:
+ */
+public class Juc02 {
+    private static int a = 0;
+    private static int b = 0;
+    public static void main(String[] args) {
+
+
+    }
+}
